6d4f2884e9f30ce3f50e24d85f231c8016084ca4
eloisedep <97959317+eloisedep@users.noreply.github.com>
2023-03-29 08:41:33 +0200

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio Eloïse de Peretti</title>
+</head>
+<body>
+	<div id="navbar">
+		<a href="#home">Home</a>
+		<a href="#news">News</a>
+		<a href="#contact">Contact</a>
+		<a href="#contact">Veille technologique</a>
+	  </div>
+</body>
+</html>
